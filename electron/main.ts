9e3cb702f8204271f59cb96700c8357d3ef5aeb2
JonesInForCode <147700520+JonesInForCode@users.noreply.github.com>
2025-04-16 10:28:55 -0700

    trying to get electron to run

---
+++ electron/main.ts
@@ -0,0 +1,20 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      contextIsolation: true,
+    }
+  });
+
+  win.loadURL('http://127.0.0.1:3000');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
